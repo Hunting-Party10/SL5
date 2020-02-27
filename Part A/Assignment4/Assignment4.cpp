ad16501be5edc54709f364d0c7035182c71bc201
Atharva Joshi <athu200299@gmail.com>
2020-02-27 16:52:49 +0530

    Lexical Analyzer Complete

--- Part A/Assignment4/Assignment4.cpp
+++ Part A/Assignment4/Assignment4.cpp
@@ -17,24 +17,87 @@ private:
 		string type;
 	}UST;
 	
-	vector<string> literal_table,symbol_table;
+	vector<string> literal_table,identifier_table;
 	std::vector<UST> ust;
 public:
 	LexicalAnalyzer(char *file);
 	bool Analyze();
+    bool testIdentifier(string,int&,int&);
+    bool testLiteral(string, int&, int&);
+    inline int testKeyword(string word){return terminal_table[word];}
 	void display();
+    void removeSpace(string&);
+    int searchIdentifier(string);
 };
+
+int LexicalAnalyzer::searchIdentifier(string l)
+{
+    for(int i =0;i<identifier_table.size();i++)
+        if(identifier_table[i].compare(l) == 0)
+            return i;
+    return -1;
+}
+
+void LexicalAnalyzer::removeSpace(string &line)
+{
+    string temp ;
+    for(int i = 0;i<line.size();i++)
+    {
+        if(line[i] != ' ')
+            temp += line[i];
+    }
+    line = temp;
+}
+
+bool LexicalAnalyzer::testIdentifier(string word,int &pos1, int &pos2)
+{
+    int prev_token = testKeyword(ust[ust.size() - 1].symbol);
+    int next_token = testKeyword(word.substr(pos1+pos2,1));
+    string w = word.substr(pos1,pos2);
+    if(w[0] - 48 <= 10)
+        return false;
+    if(prev_token != 0 && next_token != 0)
+        return true;
+    return false;
+}
+
+bool LexicalAnalyzer::testLiteral(string word,int &pos1,int &pos2)
+{
+    int prev_token = testKeyword(ust[ust.size() - 1].symbol);
+    int next_token = testKeyword(word.substr(pos1+pos2,1));
+    string w = word.substr(pos1,pos2);
+    if((prev_token == 13 || prev_token == 5 || prev_token == 6 || prev_token == 25 ||prev_token == 19 || prev_token == 7) && (next_token == 10 || next_token == 11 ||next_token == 8))// Checking for numbers
+    {
+        int flag =0;
+        for(int i=0;i<w.size();i++)
+        {
+            if(w[i] - 48 > 10)
+                flag = 1;
+        }
+        if(flag == 0)
+            return true;
+    } 
+    if(prev_token == 24 && next_token == 24 ) //string enclosed in quotes
+        return true;
+        
+    return false;
+}
 
 void LexicalAnalyzer::display()
 {
-	cout<<"Display UST\nIndex    Symbol    Type\n";
+	cout<<"Displaying UST\nIndex    Symbol    Type\n";
 	for(int i = 0;i<ust.size();i++)
 		cout<<ust[i].index<<"    "<<ust[i].symbol<<"    "<<ust[i].type<<"\n";
 	
-
+    cout<<"\n\n\n\nDisplaying Identifier table\nIndex    Symbol\n";
+    for (int i = 0; i < identifier_table.size(); ++i)
+	{
+		cout<<i<<"    "<<identifier_table[i]<<"\n";
+	}
+	cout<<"\n\n\n\nDisplaying Literal table\nIndex    Symbol\n";
 	for (int i = 0; i < literal_table.size(); ++i)
 	{
-		/* code */
+		cout<<i<<"    "<<literal_table[i]<<"\n";
 	}
 
 }
@@ -44,50 +107,66 @@ bool LexicalAnalyzer::Analyze(){
 
 	while(input.eof() != true)
 	{
-		int pointer1 = 0,pointer2 = 1;
-		string line,word;
+		int pointer1 = 0,len = 1;
+		string line;
 		getline(input,line);
-		cout<<line<<"\n";
-		while(pointer2 <= line.length())
+        //removeSpace(line);
+        cout<<"For Line :"<<line<<"\n";
+		while(pointer1 + len  <= line.length())
 		{
-			word = line.substr(pointer1,pointer2);
-			cout<<"Current Word : "<<word<<"\n";
-			UST u;
-			if(terminal_table[word] != 0)
-			{
-				//Word is a Terminal
-				
-				u.index = terminal_table[word];
-				u.symbol = word;
-				u.type = "TERM";
-				ust.push_back(u);
-				pointer1 = pointer2 ;
-			}
-			else if(terminal_table[line.substr(pointer2,pointer2+1)] != 0)
-			{
-				//Add to Symbol table0
-				if(ust[ust.size() -1].type.compare("TERM") == 0 )
-				{
-					literal_table.push_back(word);
-					u.index = literal_table.size()-1;
-					u.type = "LITERAL";
-					u.symbol = word;
-					ust.push_back(u);
-				}
-				else
-				{
-					symbol_table.push_back(word);
-					u.index = symbol_table.size()-1;
-					u.type = "Identifier";
-					u.symbol = word;
-					ust.push_back(u);
-				}
-				pointer1 = pointer2 ;	
-			}
-
-			pointer2 ++;
-		}
-		if(pointer1 + 1 != pointer2)
+            cout<<"Current Word "<<"("<<pointer1<<","<<len<<")"<<" :"<<line.substr(pointer1,len)<<"\n";
+			if((line.substr(pointer1, len)).compare(" ") == 0)
+            {
+                pointer1++;
+                continue;
+            }
+            if(testKeyword(line.substr(pointer1,len)))
+            {
+                UST u;
+                cout<<"Detected Terminal "<<"("<<pointer1<<","<<len<<")"<<" :"<<line.substr(pointer1,len)<<"\n\n\n";
+                u.symbol = line.substr(pointer1,len);
+                u.type = "TERM";
+                u.index = testKeyword(line.substr(pointer1,len));
+                ust.push_back(u);
+                pointer1 += len;
+                len = 1;
+                continue;
+            }
+            if(testLiteral(line,pointer1,len))
+            {
+                UST u;
+                cout<<"Detected Literal "<<"("<<pointer1<<","<<len<<")"<<" :"<<line.substr(pointer1,len)<<"\n\n\n";
+                u.symbol = line.substr(pointer1,len);
+                u.type = "LITERAL";
+                literal_table.push_back(u.symbol);
+                u.index = literal_table.size() -1;
+                ust.push_back(u);
+                pointer1 += len;
+                len = 1;
+                continue;
+            }
+            if(testIdentifier(line,pointer1,len))
+            {
+                UST u;
+                cout<<"Detected Identifier "<<"("<<pointer1<<","<<len<<")"<<" :"<<line.substr(pointer1,len)<<"\n\n\n";
+                
+                u.symbol = line.substr(pointer1,len);
+                u.type = "IDENTIFIER";
+                u.index = searchIdentifier(u.symbol);
+                if(u.index == -1)
+                {
+                    identifier_table.push_back(u.symbol);
+                    u.index = identifier_table.size() -1;
+                }
+                ust.push_back(u);
+                pointer1 += len;
+                len = 1;
+                continue;
+            }
+            
+            len++;
+        }
+		if(pointer1 != line.length())
 		{
 			cout<<"Syntax Error\n";
 			return false;
--- Part A/Assignment4/test.c
+++ Part A/Assignment4/test.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 #include<conio.h>
-
 int main()
 {
-	/* code */
-	int i,j,k=2,flag;
-	for ( i = 0; i < 4; i++)
-	{
-		k = i*k*j
-	}
-	flag = 12;
-	if (flag == 11)
-	{
-		printf("Hello All\n");
-	}
-	getch();
-	return 0;
+int i,j,k=2,flag;
+for ( i = 0; i < 4; i=i+1)
+{
+k = i*k*j
+}
+flag_test = 12;
+if (flag == 11)
+{
+printf("Hello All\n");
+}
+getch();
+return (0);
 }
